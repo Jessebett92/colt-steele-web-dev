6e1f1d341e0681481d7bb5715faa5fe02db988b5
jessebettridge <jessebett92@gmail.com>
2019-09-14 22:15:00 -0600

    toggle practice

---
+++ color-changer/toggle.js
@@ -0,0 +1,12 @@
+let button = document.querySelector("button");
+let isPurple = false;
+
+button.addEventListener("click", function() {
+  if (isPurple) {
+    document.body.style.background = "white";
+    isPurple = false;
+  } else {
+    document.body.style.background = "purple";
+    isPurple = true;
+  }
+});
