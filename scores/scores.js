08a1c1a0a77d97a46f396e8231cf403403dbb54c
jessebettridge <jessebett92@gmail.com>
2019-09-18 18:50:07 -0600

    30 more mins of legacy JS practice

--- scores/scores.js
+++ scores/scores.js
@@ -2,15 +2,53 @@ let p1Button = document.getElementById("p1");
 let p2Button = document.getElementById("p2");
 let p1Display = document.getElementById("player1");
 let p2Display = document.getElementById("player2");
+let resetButton = document.getElementById("reset");
+let numInput = document.querySelector("input");
+let p = document.querySelector("p");
+let winningScoreDisplay = document.querySelector("p span");
 let p1Score = 0;
 let p2Score = 0;
+let gameOver = false;
+let winningScore = 7;
 
 p1Button.addEventListener("click", () => {
-  p1Score++;
-  p1Display.textContent = p1Score;
+  if (!gameOver) {
+    p1Score++;
+    if (p1Score === winningScore) {
+      p1Display.classList.add("winner");
+      gameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
 });
 
 p2Button.addEventListener("click", () => {
-  p2Score++;
-  p2Display.textContent = p2Score;
+  if (!gameOver) {
+    p2Score++;
+    if (p2Score === winningScore) {
+      p2Display.classList.add("winner");
+      gameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  reset();
+});
+
+const reset = () => {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+};
+
+numInput.addEventListener("change", () => {
+  winningScoreDisplay.textContent = numInput.value;
+  winningScore = Number(numInput.value);
+  reset();
 });
