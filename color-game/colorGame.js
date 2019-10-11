dfd36fb2eef6242ad69dc9246ef0c44c10bb7538
jessebettridge <jessebett92@gmail.com>
2019-10-10 19:05:16 -0600

    randomized rgb color squares

--- color-game/colorGame.js
+++ color-game/colorGame.js
@@ -1,16 +1,10 @@
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-];
+let colors = generateRandomColors(6);
 
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,10 +13,11 @@ for (let i = 0; i < squares.length; i++) {
 
   squares[i].addEventListener("click", () => {
     let clickedColor = squares[i].style.backgroundColor;
-
+    console.log(clickedColor, pickedColor);
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       squares[i].style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
@@ -39,4 +34,20 @@ const changeColors = color => {
 function pickColor() {
   let random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  let arr = [];
+  for (let i = 0; i <= num; i++) {
+    arr.push(randomColor());
+  }
+
+  return arr;
+}
+
+function randomColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
